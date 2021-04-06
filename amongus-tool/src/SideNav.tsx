68289e6f832e99091a9930258db72b81b77652d6
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-04-07 00:30:27 +0900

    add players status count

--- amongus-tool/src/SideNav.tsx
+++ amongus-tool/src/SideNav.tsx
@@ -3,9 +3,28 @@ import './index.css';
 export default function SideNav(props:any) {
 	console.log(props)
 	console.log(props.crewStatuses["black"].name)
+	let life_count = 0;
+	let button_count = 0;
+	
+	props.crew.forEach((crew:any) =>{
+		if(props.crewStatuses[crew].life === false){
+			life_count += 1
+			button_count += props.crewStatuses[crew].button === false ? 1 : 0
+		}
+	})
+
 	return (
 		<div className="ly_side-nav_container">
 			<div className="ly_side-nav_items">
+				<div className="aaaa">
+					<div>
+						<i className="far fa-meh"></i> {life_count}
+					</div>
+					<div>
+						<i className="fas fa-phone"></i> {button_count}
+					</div>
+					
+				</div>
 			{
 				props.crew.map((crew:string, index:number) =>{
 					return (
